dbb2c93bf5bce30e3f06d411646fa5b05181922c
DaniAyala3103 <elias.ayala@udea.edu.co>
2026-05-22 18:53:44 -0500

    vamos dejando listo nivel 1, tanto en h como en cpp, xd

--- Tiro con Arco Atlantis/Proyecto-Final-Informatica-II-UdeA/Nivel/nivel1.cpp
+++ Tiro con Arco Atlantis/Proyecto-Final-Informatica-II-UdeA/Nivel/nivel1.cpp
@@ -1,3 +1,15 @@
 #include "nivel1.h"
 
-nivel1::nivel1() {}
+nivel1::nivel1() {
+    id =1;
+    gravedad=9.8;
+
+}
+void nivel1::iniciarNivel()
+{
+    configurarFisica();
+}
+void nivel1::actualizarNivel()
+{
+
+}
--- Tiro con Arco Atlantis/Proyecto-Final-Informatica-II-UdeA/Nivel/nivel1.h
+++ Tiro con Arco Atlantis/Proyecto-Final-Informatica-II-UdeA/Nivel/nivel1.h
@@ -1,10 +1,14 @@
 #ifndef NIVEL1_H
 #define NIVEL1_H
+#include "nivel.h"
 
-class nivel1
+class nivel1 : public Nivel
 {
 public:
     nivel1();
+    void configurarFisica();
+    void iniciarNivel();
+    void actualizarNivel();
 };
 
 #endif // NIVEL1_H
